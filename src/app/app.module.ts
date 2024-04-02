799aa95a99d51165c945b148495c5b222b66ff2b
FelixTwoli <71609611+FelixTwoli@users.noreply.github.com>
2024-04-03 00:42:38 +0300

    initial folder structure and firebase set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDXXv6qxcdxOMzfL-jXunx-3E28TBaiegg',
+    authDomain: 'connectexp-fad4e.firebaseapp.com',
+    projectId: 'connectexp-fad4e',
+    storageBucket: 'connectexp-fad4e.appspot.com',
+    messagingSenderId: '692161966333',
+    appId: '1:692161966333:web:01703903f7ba3cc31b4e94',
+  },
+};
