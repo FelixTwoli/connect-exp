7b5e95292a4cbc402ec127e0fa771494a71843d0
FelixTwoli <71609611+FelixTwoli@users.noreply.github.com>
2024-04-03 11:27:55 +0300

    add primeflex, primeicons, and primeng packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,17 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
